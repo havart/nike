b60aeafd8329cd23b43a2af74702720a44e06f6d
alex <itgavart@gmail.com>
2023-11-02 16:35:39 +0100

    init

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+  standalone: true,
+})
+export class SignUpComponent {}
