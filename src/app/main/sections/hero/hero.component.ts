ba554f3bdb8002c1170169aaf01177a873ffafc9
alex <itgavart@gmail.com>
2023-11-03 20:57:38 +0100

    fix: added button type

--- src/app/main/sections/hero/hero.component.ts
+++ src/app/main/sections/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { shoes, statistics } from '../../constant';
 import { IShoe } from '../../components/shoe-card/shoe.interface';
+import { BtnType } from '../../ui/btn.type';
 
 @Component({
   selector: 'app-hero',
@@ -12,6 +13,7 @@ export class HeroComponent {
   public statistics = statistics;
   public shoes = shoes;
   public selectedShoe = shoes[0];
+  public btnType = BtnType.PRIMARY;
 
   public onShoeSelect(shoe: IShoe): void {
     this.selectedShoe = shoe;
