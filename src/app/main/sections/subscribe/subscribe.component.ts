476ab6907b543c71fdae08f9fb2e7ebd6b082489
alex <itgavart@gmail.com>
2023-11-03 20:59:10 +0100

    feat: subscribe section

--- src/app/main/sections/subscribe/subscribe.component.ts
+++ src/app/main/sections/subscribe/subscribe.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BtnType } from '../../ui/btn.type';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
-  styleUrls: ['./subscribe.component.scss']
+  styleUrls: ['./subscribe.component.scss'],
 })
 export class SubscribeComponent {
-
+  public btnType = BtnType.PRIMARY;
 }
