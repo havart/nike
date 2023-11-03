4d2a8e73e0ea502946318f30527a8e52f025e9eb
alex <itgavart@gmail.com>
2023-11-03 20:58:28 +0100

    feat: added customer review

--- src/app/main/sections/customer-reviews/customer-reviews.component.ts
+++ src/app/main/sections/customer-reviews/customer-reviews.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { reviews } from '../../constant';
 
 @Component({
   selector: 'app-customer-reviews',
   templateUrl: './customer-reviews.component.html',
   styleUrls: ['./customer-reviews.component.scss'],
 })
-export class CustomerReviewsComponent {}
+export class CustomerReviewsComponent {
+  public reviews = reviews;
+}
