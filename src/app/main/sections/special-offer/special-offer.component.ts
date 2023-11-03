fd3164565b71cf049130ebe007f0c2f5a39d08c7
alex <itgavart@gmail.com>
2023-11-03 20:58:02 +0100

    feat: added offer

--- src/app/main/sections/special-offer/special-offer.component.ts
+++ src/app/main/sections/special-offer/special-offer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BtnType } from '../../ui/btn.type';
 
 @Component({
   selector: 'app-special-offer',
   templateUrl: './special-offer.component.html',
-  styleUrls: ['./special-offer.component.scss']
+  styleUrls: ['./special-offer.component.scss'],
 })
 export class SpecialOfferComponent {
-
+  public btnType = BtnType.PRIMARY;
 }
