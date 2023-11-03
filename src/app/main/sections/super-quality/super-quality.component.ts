538897c30f2cc22c1c1607d008070c384874690e
alex <itgavart@gmail.com>
2023-11-03 20:56:36 +0100

    feat: added quality

--- src/app/main/sections/super-quality/super-quality.component.ts
+++ src/app/main/sections/super-quality/super-quality.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BtnType } from '../../ui/btn.type';
 
 @Component({
   selector: 'app-super-quality',
   templateUrl: './super-quality.component.html',
-  styleUrls: ['./super-quality.component.scss']
+  styleUrls: ['./super-quality.component.scss'],
 })
 export class SuperQualityComponent {
-
+  public btnType = BtnType.PRIMARY;
 }
