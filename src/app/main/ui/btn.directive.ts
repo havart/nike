74aaf48f95d02d0c940793da9a7e59d5fc3204a8
alex <itgavart@gmail.com>
2023-11-03 20:55:21 +0100

    feat: added button types

--- src/app/main/ui/btn.directive.ts
+++ src/app/main/ui/btn.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
+import { BtnType } from './btn.type';
 
 @Directive({
   selector: '[btn]',
   standalone: true,
 })
 export class BtnDirective {
+  @Input()
+  public type: BtnType = BtnType.DEFAULT;
   @HostBinding('class')
-  private twClasses =
-    'flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red';
+  public get twClasses() {
+    return `flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+      this.type === BtnType.PRIMARY
+        ? 'text-white border-coral-red bg-coral-red'
+        : 'bg-white border-slate-gray text-slate-gray'
+    }`;
+  }
 
   constructor() {}
 }
